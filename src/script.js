3bfc6a8cea98cf0ac67b39800bf76071e9c80f3a
cedcoss <shubhambajpai@cedcoss.com>
2022-02-10 11:54:55 +0530

    filtertable

--- src/script.js
+++ src/script.js
@@ -36,67 +36,121 @@ var products = [{
 }];
 
 
+var html =" <div id='main'> \
+    <select id='brand'>\
+        <option value='all'>All</option>\
+        <option value='Apple'>Apple</option>\
+        <option value='Motorola'>Motorola</option>\
+        <option value='ASUS'>ASUS</option>\
+        <option value='Samsung'>Samsung</option>\
+        <option value='Microsoft'>Microsoft</option>\
+    </select>\
+    <select id='os'>\
+        <option value='all'>All</option>\
+        <option value='Android'>Android</option>\
+        <option value='iOS'>iOS</option>\
+        <option value='Windows'>Windows</option>\
+    </select>\
+    <input id='search' placeholder='search'>\
+    <div id='table'>\
+    </div>\
+</div>";
+
+$("#wrapper").html(html);
+
+var selectedOs ="all";
+var selectedBrand ="all";
+
+display(products);
+
+
 $(document).ready(function(){
-    var html =" <div id='main'> \
-                        <select id='brand'>\
-                        <option value='Apple'>Apple</option>\
-                        <option value='Motorola'>Motorola</option>\
-                        <option value='ASUS'>ASUS</option>\
-                        <option value='Samsung'>Samsung</option>\
-                        <option value='Microsoft'>Microsoft</option>\
-                         </select>\
-                         <select id='os'>\
-                        <option value='Andriod'>Andriod</option>\
-                        <option value='iOS'>iOS</option>\
-                        <option value='Windows'>Windows</option>\
-                        </select>\
-                    <div id='table'>\
-                    </div>\
-               </div>";
+       
+     //filter by brand
+     $("select#brand").on("change",function(){
+        selectedBrand = $(this).children("option:selected").val();
+        console.log(selectedOs);
+        console.log(selectedBrand);
+        filterVals(selectedBrand,selectedOs);
 
-                    
-    $("#wrapper").html(html);
+    });
 
+
+    //filter by os
+    $("select#os").change(function(){
+        selectedOs = $(this).children("option:selected").val();
+        console.log(selectedOs);
+        console.log(selectedBrand);
+        filterVals(selectedBrand,selectedOs);
+    });
+
+    //search the product
+    $('input#search').on('keyup',function(){
+        var value = $(this).val();
+        $("tr").filter(function(){
+            $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1);
+            });
+    });
+
+
+    //show/hide particular row
+        $(".close").on('click',function(){
+            $(this).parents("tr").toggle();
+        });
+
+});
+
+
+
+//display table
+function display(filterproduct){
     var row = " <tr>\<th>Product Id</th><th>Product Name</th><th>Product Brand</th><th>Product Os</th></tr>";
-    
-    for(let i=0;i<products.length;i++)
-        row +='<tr><td>'+products[i].id+'</td><td>'+products[i].name+'</td><td>'+products[i].brand+'</td><td>'+products[i].os+'</td><td><a href="#" class="close">X</a></td></tr>';
+    for(let i=0;i<filterproduct.length;i++)
+        row +='<tr><td>'+filterproduct[i].id+'</td><td>'+filterproduct[i].name+'</td><td>'+filterproduct[i].brand+'</td><td>'+filterproduct[i].os+'</td><td><a href="#" class="close">X</a></td></tr>';
 
     $("#table").html("<table>"+row+"</table>");
-    
 
-    //filter by brands
-    $("select#brand").change(function(){
-       var brand = $(this).children("option:selected").val();
-       $("td").filter(function(){
-       return $(this).text().indexOf(brand) !== -1;
-       }).parent().remove();
-    
-    });
+}
 
 
 
-    //filter by OS
-    $("select#os").change(function(){
-        var os = $(this).children("option:selected").val();
-        console.log(os);
-        $("td").filter(function(){
-            return $(this).text().indexOf(os) == -1;
-        }).parent().remove();
+//fetching filter values
+function filterVals(selectedBrand,selectedOs){
+    var displayProduct =[];
 
-    });
+   if(selectedBrand=='all' && selectedOs=='all'){
+       display(products);
+   }
 
- });
+   else if(selectedBrand=='all' &&  selectedOs !=='all'){
+       for(var i=0;i<products.length;i++){
+           if(products[i].os === selectedOs){
+               displayProduct.push(products[i]);
+           }
+       }
+     display(displayProduct);
  
+   }
+   else if(selectedOs=='all' && selectedBrand !=='all'){
+    for(var i=0;i<products.length;i++){
+        if(products[i].brand == selectedBrand){
+            displayProduct.push(products[i]);
+        }
+    }
+  
+   display(displayProduct);
+   }
+  else{
+    for(var i=0;i<products.length;i++){
+        if(products[i].brand === selectedBrand && products[i].os === selectedOs){
+            displayProduct.push(products[i]);
+        }
+    }
 
+  display(displayProduct);
 
+  }
 
   
-
-
-
-
-
-
-
+}
      
